cab48f9592b9ab0bc65ad2d1415ff35316f4631e
albert874 <140815215+albert874@users.noreply.github.com>
2023-07-28 19:22:34 +0700

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 		$uri = 'http://';
 	}
 	$uri .= $_SERVER['HTTP_HOST'];
-	header('Location: '.$uri.'/tugas/dashboard.php');
+	header('Location: '.$uri.'/dashboard');
 	exit;
 ?>
 Something is wrong with the XAMPP installation :-(
